b971e4112417788cfdc8425fc6622d4d8b34a62a
MollyHitsDifferent <molly.palocy@gmail.com>
2026-03-04 11:44:41 -0500

    HAving trouble with Enum Behaviour. Fighter Array Laid out.

---
+++ src/main/java/TeachingMyselfArrays/Dice.java
@@ -0,0 +1,23 @@
+// import java.util.stream.IntStream;
+
+
+
+public enum Dice {
+    d4(new int[] {1,2,3,4}),
+    d6(new int[] {1,2,3,4,5,6}),
+     d8(new int[] {1,2,3,4,5,6,7,8}),
+      d10(new int[] {1,2,3,4,5,6,7,8,9,10}),
+       d12(new int[] {1,2,3,4,5,6,7,8,9,10,11,12}),
+        d20(new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}),
+
+    private int[] range;
+
+    private DieRolls(int[] inputRange){
+        this.range = inputRange;
+    }
+    public int[] getRange() {
+        return range;
+    }
+   
+}
+
--- src/main/java/TeachingMyselfArrays/Fighter.java
+++ src/main/java/TeachingMyselfArrays/Fighter.java
@@ -1,28 +1,19 @@
 import java.util.*;
 import java.util.stream.IntStream;
+
 public class Fighter {
     private String hitDie;
-    private DieRolls DamDie;
+    private Dice damDie;
     private int[] damRange;
     private int healthPoints;
     private String name;
 
-    public Fighter(String hd, DieRolls dd, int hp, String name) {
+    public Fighter(String hd, Dice dd, int hp, String name) {
         this.hitDie = hd;
-        this.DamDie = dd
-        this.damRange = DieRolls(this.DamDie);
+        this.damDie = dd;
+        this.damRange = damDie.getRange();
         this.healthPoints = hp;
         this.name = name;
-    }
-    public int[] DieRolls(DieRolls x){
-    String dicetoString = x.toString();
-    int holder = Integer.parseInt(dicetoString);
-    holder++;
-    int[] damageRange = IntStream.rangeClosed(1, holder).toArray();
-        return damageRange;
-}
-    public int[] getDamRange() {
-        return damRange;
     }
 
     public String getName() {
@@ -39,12 +30,19 @@ public class Fighter {
 
     @Override
     public String toString() {
-        return "Fighter name: " + this.getName() + " has " + getHealthPoints() + " HP.";
+        return "Fighter name: " + this.getName() + " has " + this.getHealthPoints() + " HP.";
     }
+
     public static void main(String[] args) {
         // So you DONT USE NEW when you set up a stream for some reason. When you are looking to store
         // a stream as an array instantiate the variable as an arry, then stream to an array 
-        int[] d8 = IntStream.range(1,9).toArray();
-        Fighter J = new Fighter("d10" , d4 , 22, "Roarger");
+        Fighter[] fiteList = new Fighter[
+        new Fighter("d10" , Dice.d4, 22, "Roarger"),
+        new Fighter("d10" , Dice.d4, 22, "boarger"),
+        new Fighter("d10" , Dice.d4, 22, "Coarger"),
+        new Fighter("d10" , Dice.d4, 22, "Toarger"),
+        new Fighter("d10" , Dice.d4, 22, "XDoarger")
+    ]
+        System.out.println(J);
     }
 }
