9706081725e20b9cdd6878aef38a2c787a5fbd3c
MollyHitsDifferent <molly.palocy@gmail.com>
2026-03-04 12:10:33 -0500

    Stream is working Stream filter, stream for Each. Need to play with For Each.

---
+++ src/main/java/TeachingMyselfArrays/FightLister.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class FightLister implements Iterable {
+
+    private Fighter[] fightlist;
+
+    public FightLister(int number){
+        this.fightlist = new ArrayList<Fighter>;
+    }
+
+}
--- src/main/java/TeachingMyselfArrays/Fighter.java
+++ src/main/java/TeachingMyselfArrays/Fighter.java
@@ -37,12 +37,21 @@ public class Fighter {
         // So you DONT USE NEW when you set up a stream for some reason. When you are looking to store
         // a stream as an array instantiate the variable as an arry, then stream to an array 
         Fighter[] fiteList = {
-        new Fighter("d10" , Dice.d4, 22, "Roarger"),
-        new Fighter("d10" , Dice.d4, 22, "boarger"),
-        new Fighter("d10" , Dice.d4, 22, "Coarger"),
-        new Fighter("d10" , Dice.d4, 22, "Toarger"),
-        new Fighter("d10" , Dice.d4, 22, "XDoarger")
-        ;}
-        System.out.println(fiteList);
+        new Fighter("d10" , Dice.d4, 9, "Roarger"),
+        new Fighter("d10" , Dice.d4, 66, "boarger"),
+        new Fighter("d10" , Dice.d4, 14, "Coarger"),
+        new Fighter("d10" , Dice.d4, 13, "Toarger"),
+        new Fighter("d10" , Dice.d4, 8, "XDoarger")};
+        System.err.println("Full List");
+        Arrays.stream(fiteList).forEach(System.out::println);
+            System.out.println("Filter List");
+        Arrays.stream(fiteList).filter(e -> e.getHealthPoints() >10).forEach(System.out::println);
+
+        // this is a way to print each value without using any tools. Its not what they want me to do.
+        // int counter = 0 ;
+        // while (counter < 5){
+        //     System.err.println(fiteList[counter]);
+        //     counter++ ;
+        // }
     }
 }
