05f22a8b6ffa09c2ff048010278ca149c2992166
MollyHitsDifferent <molly.palocy@gmail.com>
2026-03-11 12:32:12 -0400

    Morbius working

--- src/main/java/Midterm2Practicum/Morbius.java
+++ src/main/java/Midterm2Practicum/Morbius.java
@@ -1,16 +1,29 @@
 package main.java.Midterm2Practicum;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Morbius {
 
     public static void main(String[] args) {
-    // String filename = "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/morbius.txt";
-       FileReader FR =  new FileReader("morbius.txt");
-       BufferedReader BR = new BufferedReader(FR);
-       System.out.println(BR);
+        // System.out.println( new file.ge);
+        // Scanner scanner = new
+        // Scanner("/src/main/java/Midterm2Practicum/morbius.txt");
+        try {
+            int x = 0;
+            FileReader FR = new FileReader(
+                    "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
+            BufferedReader BR = new BufferedReader(FR);
+           System.out.println( 
+            BR.lines().filter(str -> str.toLowerCase().contains("morbius")).count()
+        );
+            // .forEach(System.out::println);;
+
+        } catch (FileNotFoundException E) {
+            System.out.println("Im an angry girl" + E);
+        }
 
     }
-
 }
