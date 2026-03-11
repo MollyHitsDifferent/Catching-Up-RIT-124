24d6adea530e6cb296b405526f16cbd62c3b4f48
MollyHitsDifferent <molly.palocy@gmail.com>
2026-03-11 13:06:11 -0400

    Morbius Complete doing Java practice problems from Gemini

--- src/main/java/Midterm2Practicum/Morbius.java
+++ src/main/java/Midterm2Practicum/Morbius.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Morbius {
 
@@ -11,19 +12,31 @@ public class Morbius {
         // System.out.println( new file.ge);
         // Scanner scanner = new
         // Scanner("/src/main/java/Midterm2Practicum/morbius.txt");
+        int x = 0;
         try {
-            int x = 0;
             FileReader FR = new FileReader(
-                    "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
+            "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
             BufferedReader BR = new BufferedReader(FR);
-           System.out.println( 
-            BR.lines().filter(str -> str.toLowerCase().contains("morbius")).count()
-        );
-            // .forEach(System.out::println);;
+            Scanner scanner = new Scanner(BR);
+            scanner.useDelimiter("\\W+");
+                        while (scanner.hasNext()) {
+                if (scanner.next().equalsIgnoreCase("morbius")) {
+                    x++;
+                }
+            }
+            // int x = 0;
+            // FileReader FR = new FileReader(
+            // "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
+            // BufferedReader BR = new BufferedReader(FR);
+            // Stream<String> line = BR.lines().filter(str ->
+            // str.toLowerCase().contains("morbius")).map(String x ->
+            // x.split().equals("morbius").count(););
+            // System.err.println(line);
+            // // .forEach(System.out::println);;
 
-        } catch (FileNotFoundException E) {
-            System.out.println("Im an angry girl" + E);
+        }catch (FileNotFoundException E) {System.out.println( "Error " + E);}
+         finally {
+            System.out.println(x);
         }
-
     }
 }
