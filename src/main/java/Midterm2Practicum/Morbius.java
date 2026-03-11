a8d00d07971d4488bed10af36bdce3f8d1dbe7ac
MollyHitsDifferent <molly.palocy@gmail.com>
2026-03-11 13:59:35 -0400

    Exercise 2  on Practicum Midterm 2. Still returning text after : even after switching to .contains

--- src/main/java/Midterm2Practicum/Morbius.java
+++ src/main/java/Midterm2Practicum/Morbius.java
@@ -8,35 +8,49 @@ import java.util.stream.Stream;
 
 public class Morbius {
 
+    public static void stageDirections(String filename) {
+        try {
+            FileReader Fr = new FileReader(filename);
+            BufferedReader BR = new BufferedReader(Fr);
+            Scanner scanner = new Scanner(BR);
+            
+            while (scanner.hasNext()) {
+                String checker = scanner.next();
+                if (checker.contains("\n" + "[")){
+                    String holder = "";
+                    holder = holder + checker + " ";
+                while (!checker.contains("]") && scanner.hasNext()){
+                    checker = scanner.next();
+                    holder = holder + checker + " ";
+                }
+System.out.println(holder);
+                }
+            }
+        }
+        catch (FileNotFoundException E ) {
+            System.out.println("MADMADMAD " + E);
+        }
+    }
     public static void main(String[] args) {
-        // System.out.println( new file.ge);
-        // Scanner scanner = new
-        // Scanner("/src/main/java/Midterm2Practicum/morbius.txt");
         int x = 0;
         try {
             FileReader FR = new FileReader(
-            "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
+                    "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
             BufferedReader BR = new BufferedReader(FR);
             Scanner scanner = new Scanner(BR);
             scanner.useDelimiter("\\W+");
-                        while (scanner.hasNext()) {
+            while (scanner.hasNext()) {
                 if (scanner.next().equalsIgnoreCase("morbius")) {
                     x++;
                 }
             }
-            // int x = 0;
-            // FileReader FR = new FileReader(
-            // "/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
-            // BufferedReader BR = new BufferedReader(FR);
-            // Stream<String> line = BR.lines().filter(str ->
-            // str.toLowerCase().contains("morbius")).map(String x ->
-            // x.split().equals("morbius").count(););
-            // System.err.println(line);
-            // // .forEach(System.out::println);;
 
-        }catch (FileNotFoundException E) {System.out.println( "Error " + E);}
-         finally {
+        } catch (FileNotFoundException E) {
+            System.out.println("Error " + E);
+        } finally {
             System.out.println(x);
         }
+        stageDirections("/Users/mollypalocy/SoftDevII/Catching-Up-RIT-124/src/main/java/Midterm2Practicum/morbius.txt");
+
     }
 }
